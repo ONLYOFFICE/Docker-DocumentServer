1125fc0900488e9e8f5d208fd489e3e6d672b91d
Alexey.Golubev <alexey.golubev@onlyoffice.com>
2016-06-30 12:41:52 +0300

    updated for onlyoffice-documentserver v4.0

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,42 @@
 FROM ubuntu:14.04
 MAINTAINER Ascensio System SIA <support@onlyoffice.com>
 
-ENV LANG en_US.UTF-8  
-ENV LANGUAGE en_US:en  
-ENV LC_ALL en_US.UTF-8 
-
-RUN apt-get update && apt-get -y -q install libreoffice 
+ENV LANG=en_US.UTF-8 LANGUAGE=en_US:en LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive
 
 RUN echo "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d && \
-    echo "deb http://static.teamlab.com.s3.amazonaws.com/repo/debian/ squeeze main" >>  /etc/apt/sources.list && \
+    apt-get -y update && \
+    apt-get --force-yes -yq install apt-transport-https && \
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D9D0BF019CC8AC0D && \
-    echo "deb http://download.mono-project.com/repo/debian wheezy/snapshots/3.12.0  main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list && \
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF && \
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1655A0AB68576280 && \
     echo "deb http://archive.ubuntu.com/ubuntu precise main universe multiverse" >> /etc/apt/sources.list && \
-    DEBIAN_FRONTEND=noninteractive  && \
+    echo "deb https://deb.nodesource.com/node_4.x trusty main" | tee /etc/apt/sources.list.d/nodesource.list && \
     locale-gen en_US.UTF-8 && \
     apt-get -y update && \
     echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections && \
     apt-get install --force-yes -yq software-properties-common && \
     add-apt-repository ppa:ubuntu-toolchain-r/test && \
     apt-get -y update && \
-    apt-get --force-yes -yq install gcc-4.9 onlyoffice-documentserver nano htop && \
+    apt-get --force-yes -yq install software-properties-common adduser mysql-server redis-server rabbitmq-server nginx-extras nodejs libstdc++6 libcurl3 libxml2 libboost-regex-dev zlib1g supervisor fonts-dejavu fonts-liberation ttf-mscorefonts-installer fonts-crosextra-carlito fonts-takao-gothic fonts-opensymbol libxss1 libgtkglext1 libcairo2 xvfb libxtst6 libgconf2-4 libasound2 bomstrip libnspr4 libnss3 libnss3-nssdb nano htop && \
+    service mysql stop && \
+    service redis-server stop && \
+    service rabbitmq-server stop && \
+    service supervisor stop && \
+    service nginx stop && \
     rm -rf /var/lib/apt/lists/*
 
 ADD config /app/onlyoffice/setup/config/
 ADD run-document-server.sh /app/onlyoffice/run-document-server.sh
-RUN chmod 755 /app/onlyoffice/*.sh
 
-VOLUME ["/var/log/onlyoffice"]
-VOLUME ["/var/www/onlyoffice/Data"]
+EXPOSE 80 443
 
-EXPOSE 80
-EXPOSE 443
+RUN echo "deb http://static.teamlab.com/repo/debian/ squeeze main" | tee /etc/apt/sources.list.d/onlyoffice.list && \
+    apt-get -y update && \
+    service mysql start && \
+    apt-get --force-yes -yq install onlyoffice-documentserver && \
+    service mysql stop && \
+    chmod 755 /app/onlyoffice/*.sh && \
+    rm -rf /var/lib/apt/lists/*
+
+VOLUME /etc/onlyoffice /var/log/onlyoffice /var/lib/onlyoffice /var/www/onlyoffice/Data
 
 CMD bash -C '/app/onlyoffice/run-document-server.sh';'bash'
--- run-document-server.sh
+++ run-document-server.sh
@@ -1,76 +1,139 @@
 #!/bin/bash
 
-sed "/user=/s/onlyoffice/root/" -i /etc/supervisor/conf.d/CoAuthoringService.conf
-sed "/user=/s/onlyoffice/root/" -i /etc/supervisor/conf.d/DocService.conf
-sed "/user=/s/onlyoffice/root/" -i /etc/supervisor/conf.d/FileConverterService.conf
-sed "/user=/s/onlyoffice/root/" -i /etc/supervisor/conf.d/LibreOfficeService.conf
-sed "/user=/s/onlyoffice/root/" -i /etc/supervisor/conf.d/SpellCheckerService.conf
-
-sed "/sudo /s/-u onlyoffice//" -i /var/www/onlyoffice/documentserver/Tools/CheckDocService.sh
-sed "/sudo /s/-u onlyoffice//" -i /var/www/onlyoffice/documentserver/Tools/GenerateAllFonts.sh
-
-chown root /var/www/onlyoffice
-chown root /var/lib/onlyoffice
-
-adduser --quiet www-data root
-
+APP_DIR="/var/www/onlyoffice/documentserver"
 DATA_DIR="/var/www/onlyoffice/Data"
-LOG_DIR="/var/log/onlyoffice"
+LOG_DIR="/var/log/onlyoffice/documentserver"
 
 ONLYOFFICE_HTTPS=${ONLYOFFICE_HTTPS:-false}
 
 SSL_CERTIFICATES_DIR="${DATA_DIR}/certs"
 SSL_CERTIFICATE_PATH=${SSL_CERTIFICATE_PATH:-${SSL_CERTIFICATES_DIR}/onlyoffice.crt}
 SSL_KEY_PATH=${SSL_KEY_PATH:-${SSL_CERTIFICATES_DIR}/onlyoffice.key}
+CA_CERTIFICATES_PATH=${CA_CERTIFICATES_PATH:-${SSL_CERTIFICATES_DIR}/ca-certificates.pem}
 SSL_DHPARAM_PATH=${SSL_DHPARAM_PATH:-${SSL_CERTIFICATES_DIR}/dhparam.pem}
 SSL_VERIFY_CLIENT=${SSL_VERIFY_CLIENT:-off}
 ONLYOFFICE_HTTPS_HSTS_ENABLED=${ONLYOFFICE_HTTPS_HSTS_ENABLED:-true}
 ONLYOFFICE_HTTPS_HSTS_MAXAGE=${ONLYOFFICE_HTTPS_HSTS_MAXAG:-31536000}
 SYSCONF_TEMPLATES_DIR="/app/onlyoffice/setup/config"
 
-NGINX_ONLYOFFICE_PATH="/etc/nginx/sites-enabled/onlyoffice-documentserver";
+NGINX_ONLYOFFICE_PATH="/etc/nginx/conf.d/onlyoffice-documentserver.conf";
+
+NGINX_CONFIG_PATH="/etc/nginx/nginx.conf"
+NGINX_WORKER_PROCESSES=${NGINX_WORKER_PROCESSES:-$(grep processor /proc/cpuinfo | wc -l)}
+NGINX_WORKER_CONNECTIONS=${NGINX_WORKER_CONNECTIONS:-$(ulimit -n)}
+
+ONLYOFFICE_DEFAULT_CONFIG=/etc/onlyoffice/documentserver/default.json
+
+MYSQL_SERVER_HOST=${MYSQL_SERVER_HOST:-"localhost"}
+MYSQL_SERVER_PORT=${MYSQL_SERVER_PORT:-"3306"}
+MYSQL_SERVER_DB_NAME=${MYSQL_SERVER_DB_NAME:-"onlyoffice"}
+MYSQL_SERVER_USER=${MYSQL_SERVER_USER:-"root"}
+MYSQL_SERVER_PASS=${MYSQL_SERVER_PASS:-""}
+
+RABBITMQ_SERVER_HOST=${RABBITMQ_SERVER_HOST:-"localhost"}
+RABBITMQ_SERVER_USER=${RABBITMQ_SERVER_USER:-"guest"}
+RABBITMQ_SERVER_PASS=${RABBITMQ_SERVER_PASS:-"guest"}
+
+REDIS_SERVER_HOST=${REDIS_SERVER_HOST:-"localhost"}
+REDIS_SERVER_PORT=${REDIS_SERVER_PORT:-"6379"}
 
 # create base folders
-mkdir -p /var/log/onlyoffice/documentserver/FileConverterService/
-mkdir -p /var/log/onlyoffice/documentserver/CoAuthoringService/
-mkdir -p /var/log/onlyoffice/documentserver/DocService/
-mkdir -p /var/log/onlyoffice/documentserver/SpellCheckerService/
-mkdir -p /var/log/onlyoffice/documentserver/LibreOfficeService/
-mkdir -p /var/log/onlyoffice/documentserver/WatchDogService/
+for i in converter docservice spellchecker metrics gc; do
+	mkdir -p "${LOG_DIR}/$i"
+done
+
+mkdir -p ${LOG_DIR}-example
+
+# Set up nginx
+sed 's/^worker_processes.*/'"worker_processes ${NGINX_WORKER_PROCESSES};"'/' -i ${NGINX_CONFIG_PATH}
+sed 's/worker_connections.*/'"worker_connections ${NGINX_WORKER_CONNECTIONS};"'/' -i ${NGINX_CONFIG_PATH}
+sed 's/access_log.*/'"access_log off;"'/' -i ${NGINX_CONFIG_PATH}
 
 # setup HTTPS
 if [ -f "${SSL_CERTIFICATE_PATH}" -a -f "${SSL_KEY_PATH}" ]; then
-        cp ${SYSCONF_TEMPLATES_DIR}/nginx/onlyoffice-ssl ${NGINX_ONLYOFFICE_PATH}
+  cp ${SYSCONF_TEMPLATES_DIR}/nginx/onlyoffice-documentserver-ssl.conf ${NGINX_ONLYOFFICE_PATH}
 
-        mkdir ${DATA_DIR}
-        mkdir ${LOG_DIR}/nginx
+  mkdir ${DATA_DIR}
 
-        # configure nginx
-        sed 's,{{SSL_CERTIFICATE_PATH}},'"${SSL_CERTIFICATE_PATH}"',' -i ${NGINX_ONLYOFFICE_PATH}
-        sed 's,{{SSL_KEY_PATH}},'"${SSL_KEY_PATH}"',' -i ${NGINX_ONLYOFFICE_PATH}
+  # configure nginx
+  sed 's,{{SSL_CERTIFICATE_PATH}},'"${SSL_CERTIFICATE_PATH}"',' -i ${NGINX_ONLYOFFICE_PATH}
+  sed 's,{{SSL_KEY_PATH}},'"${SSL_KEY_PATH}"',' -i ${NGINX_ONLYOFFICE_PATH}
 
-        # if dhparam path is valid, add to the config, otherwise remove the option
-        if [ -r "${SSL_DHPARAM_PATH}" ]; then
-          sed 's,{{SSL_DHPARAM_PATH}},'"${SSL_DHPARAM_PATH}"',' -i ${NGINX_ONLYOFFICE_PATH}
-        else
-          sed '/ssl_dhparam {{SSL_DHPARAM_PATH}};/d' -i ${NGINX_ONLYOFFICE_PATH}
-        fi
+  # if dhparam path is valid, add to the config, otherwise remove the option
+  if [ -r "${SSL_DHPARAM_PATH}" ]; then
+    sed 's,{{SSL_DHPARAM_PATH}},'"${SSL_DHPARAM_PATH}"',' -i ${NGINX_ONLYOFFICE_PATH}
+  else
+    sed '/ssl_dhparam {{SSL_DHPARAM_PATH}};/d' -i ${NGINX_ONLYOFFICE_PATH}
+  fi
 
-        sed 's,{{SSL_VERIFY_CLIENT}},'"${SSL_VERIFY_CLIENT}"',' -i ${NGINX_ONLYOFFICE_PATH}
+  sed 's,{{SSL_VERIFY_CLIENT}},'"${SSL_VERIFY_CLIENT}"',' -i ${NGINX_ONLYOFFICE_PATH}
 
-        if [ -f /usr/local/share/ca-certificates/ca.crt ]; then
-          sed 's,{{CA_CERTIFICATES_PATH}},'"${CA_CERTIFICATES_PATH}"',' -i ${NGINX_ONLYOFFICE_PATH}
-        else
-          sed '/{{CA_CERTIFICATES_PATH}}/d' -i ${NGINX_ONLYOFFICE_PATH}
-        fi
+  if [ -f "${CA_CERTIFICATES_PATH}" ]; then
+    sed 's,{{CA_CERTIFICATES_PATH}},'"${CA_CERTIFICATES_PATH}"',' -i ${NGINX_ONLYOFFICE_PATH}
+  else
+    sed '/{{CA_CERTIFICATES_PATH}}/d' -i ${NGINX_ONLYOFFICE_PATH}
+  fi
 
-        if [ "${ONLYOFFICE_HTTPS_HSTS_ENABLED}" == "true" ]; then
-          sed 's/{{ONLYOFFICE_HTTPS_HSTS_MAXAGE}}/'"${ONLYOFFICE_HTTPS_HSTS_MAXAGE}"'/' -i ${NGINX_ONLYOFFICE_PATH}
-        else
-          sed '/{{ONLYOFFICE_HTTPS_HSTS_MAXAGE}}/d' -i ${NGINX_ONLYOFFICE_PATH}
-        fi
+  if [ "${ONLYOFFICE_HTTPS_HSTS_ENABLED}" == "true" ]; then
+    sed 's/{{ONLYOFFICE_HTTPS_HSTS_MAXAGE}}/'"${ONLYOFFICE_HTTPS_HSTS_MAXAGE}"'/' -i ${NGINX_ONLYOFFICE_PATH}
+  else
+    sed '/{{ONLYOFFICE_HTTPS_HSTS_MAXAGE}}/d' -i ${NGINX_ONLYOFFICE_PATH}
+  fi
+else
+  cp ${SYSCONF_TEMPLATES_DIR}/nginx/onlyoffice-documentserver.conf ${NGINX_ONLYOFFICE_PATH}
 fi
 
-service mysql start
+JSON="json -I -q -f ${ONLYOFFICE_DEFAULT_CONFIG}"
+
+if [ ${MYSQL_SERVER_HOST} != "localhost" ]; then
+
+  # Change mysql settings
+  ${JSON} -e "this.services.CoAuthoring.sql.dbHost = '${MYSQL_SERVER_HOST}'"
+  ${JSON} -e "this.services.CoAuthoring.sql.dbPort = '${MYSQL_SERVER_PORT}'"
+  ${JSON} -e "this.services.CoAuthoring.sql.dbName = '${MYSQL_SERVER_DB_NAME}'"
+  ${JSON} -e "this.services.CoAuthoring.sql.dbUser = '${MYSQL_SERVER_USER}'"
+  ${JSON} -e "this.services.CoAuthoring.sql.dbPass = '${MYSQL_SERVER_PASS}'"
+
+  MYSQL="mysql -s -h${MYSQL_SERVER_HOST} -u${MYSQL_SERVER_USER}"
+  if [ -n "${MYSQL_SERVER_PASS}" ]; then
+    MYSQL="$MYSQL -p${MYSQL_SERVER_PASS}"
+  fi
+
+  # Create  db on remote server
+  ${MYSQL} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_SERVER_DB_NAME} CHARACTER SET utf8 COLLATE 'utf8_general_ci';"
+  ${MYSQL} "${MYSQL_SERVER_DB_NAME}" < "${APP_DIR}/server/schema/createdb.sql"
+else
+  service mysql start
+fi
+
+if [ ${RABBITMQ_SERVER_HOST} != "localhost" ]; then
+
+  # Change rabbitmq settings
+  ${JSON} -e "this.rabbitmq.url = 'amqp://${RABBITMQ_SERVER_HOST}'"
+  ${JSON} -e "this.rabbitmq.login = '${RABBITMQ_SERVER_USER}'"
+  ${JSON} -e "this.rabbitmq.password = '${RABBITMQ_SERVER_PASS}'"
+  
+else
+  service redis-server start
+fi
+
+if [ ${REDIS_SERVER_HOST} != "localhost" ]; then
+
+  # Change redis settings
+  ${JSON} -e "this.services.CoAuthoring.redis.host = '${REDIS_SERVER_HOST}'"
+  ${JSON} -e "this.services.CoAuthoring.redis.port = '${REDIS_SERVER_PORT}'"
+  
+else
+  service rabbitmq-server start
+fi
+
+# Copy modified supervisor start script
+cp ${SYSCONF_TEMPLATES_DIR}/supervisor/supervisor /etc/init.d/
+# Copy modified supervisor config
+cp ${SYSCONF_TEMPLATES_DIR}/supervisor/supervisord.conf /etc/supervisor/supervisord.conf
+
 service nginx start
 service supervisor start
+
+# Regenerate the fonts list and the fonts thumbnails
+documentserver-generate-allfonts.sh
