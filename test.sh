166d218a156c5e54cc5eef884c349e69e1de589f
Semen Bezrukov <semen.bezrukov@onlyoffice.com>
2019-10-23 16:05:43 +0300

    Update Travis test

--- test.sh
+++ test.sh
@@ -9,21 +9,12 @@ fi
 # Run test environment
 docker-compose -p ds -f $file up -d
 
-wakeup_attempts=30
-wakeup_timeout=5
+wakeup_timeout=30
 
-for ((i=0; i<$wakeup_attempts; i++))
-do
-  # Get documentserver healthcheck status
-  healthcheck_res=$(wget --no-check-certificate -qO - localhost/healthcheck)
-  
-  if [[ $healthcheck_res == "true" ]]; then
-    break
-  else
-    echo "Wait for service wake up #$i"
-    sleep $wakeup_timeout
-  fi
-done
+# Get documentserver healthcheck status
+echo "Wait for service wake up"
+sleep $wakeup_timeout
+healthcheck_res=$(wget --no-check-certificate -qO - localhost/healthcheck)
 
 # Fail if it isn't true
 if [[ $healthcheck_res == "true" ]]; then
