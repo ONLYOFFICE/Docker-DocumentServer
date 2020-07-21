22bf46abec10efa5f417a65c12b996d92922ffe5
Semen Bezrukov <semen.bezrukov@onlyoffice.com>
2020-07-21 21:01:46 +0300

    Add metrics configuration

---
+++ config/statsd/config.js
@@ -0,0 +1,7 @@
+{
+  "graphiteHost": "127.0.0.1",
+  "graphitePort": 2003,
+  "port": 8125,
+  "flushInterval": 3000,
+  "backends": [ "./backends/graphite.js" ]
+}
--- run-document-server.sh
+++ run-document-server.sh
@@ -84,6 +84,11 @@ read_setting(){
   deprecated_var AMQP_SERVER_URL AMQP_URI
   deprecated_var AMQP_SERVER_TYPE AMQP_TYPE
 
+  METRICS_ENABLED="${METRICS_ENABLED:-$(${JSON} statsd.useMetrics)}"
+  METRICS_HOST="${METRICS_HOST:-$(${JSON} statsd.host)}"
+  METRICS_PORT="${METRICS_PORT:-$(${JSON} statsd.port)}"
+  METRICS_PREFIX="${METRICS_PREFIX:-$(${JSON} statsd.prefix)}"
+
   DB_HOST=${DB_HOST:-${POSTGRESQL_SERVER_HOST:-$(${JSON} services.CoAuthoring.sql.dbHost)}}
   case $DB_TYPE in
     "postgres")
@@ -185,6 +190,16 @@ waiting_for_redis(){
 waiting_for_datacontainer(){
   waiting_for_connection ${ONLYOFFICE_DATA_CONTAINER_HOST} ${ONLYOFFICE_DATA_CONTAINER_PORT}
 }
+
+update_statsd_settings(){
+  cp ${SYSCONF_TEMPLATES_DIR}/statsd/config.js ${APP_DIR}/server/Metrics/config/config.js
+
+  ${JSON} -I -e "this.statsd.useMetrics = '${METRICS_ENABLED}'"
+  ${JSON} -I -e "this.statsd.host = '${METRICS_HOST}'"
+  ${JSON} -I -e "this.statsd.port = '${METRICS_PORT}'"
+  ${JSON} -I -e "this.statsd.prefix = '${METRICS_PREFIX}'"
+}
+
 update_db_settings(){
   ${JSON} -I -e "this.services.CoAuthoring.sql.type = '${DB_TYPE}'"
   ${JSON} -I -e "this.services.CoAuthoring.sql.dbHost = '${DB_HOST}'"
@@ -425,6 +440,10 @@ done
 if [ ${ONLYOFFICE_DATA_CONTAINER_HOST} = "localhost" ]; then
 
   read_setting
+
+  if [ $METRICS_ENABLED = "true" ]; then
+    update_statsd_settings
+  fi
 
   update_welcome_page
 
