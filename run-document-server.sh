a42276b356de406994509941b3887e958eb33072
Roman <demidov.roman@onlyoffice.com>
2020-09-28 13:20:03 +0300

    Fix #281: Add option to disable CPU-heavy tasks on startup

--- run-document-server.sh
+++ run-document-server.sh
@@ -49,6 +49,8 @@ JWT_SECRET=${JWT_SECRET:-secret}
 JWT_HEADER=${JWT_HEADER:-Authorization}
 JWT_IN_BODY=${JWT_IN_BODY:-false}
 
+GENERATE_FONTS_THEMES_ENABLED=${GENERATE_FONTS_THEMES_ENABLED:-true}
+
 if [[ ${PRODUCT_NAME} == "documentserver" ]]; then
   REDIS_ENABLED=false
 else
@@ -537,7 +539,9 @@ fi
 service nginx start
 
 # Regenerate the fonts list and the fonts thumbnails
-documentserver-generate-allfonts.sh ${ONLYOFFICE_DATA_CONTAINER}
+if [ "${GENERATE_FONTS_THEMES_ENABLED}" == "true" ]; then
+  documentserver-generate-allfonts.sh ${ONLYOFFICE_DATA_CONTAINER}
+fi
 documentserver-static-gzip.sh ${ONLYOFFICE_DATA_CONTAINER}
 
 tail -f /var/log/${COMPANY_NAME}/**/*.log
