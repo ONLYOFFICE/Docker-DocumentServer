bee8dd1bb89e8c65efec031387c6888962364af0
Roman Demidov <roman.demidov@onlyoffice.com>
2020-11-18 12:29:05 +0300

    Timeout

--- tests/test.sh
+++ tests/test.sh
@@ -36,7 +36,7 @@ fi
 # Run test environment
 docker-compose -p ds -f $config up -d
 
-wakeup_timeout=60
+wakeup_timeout=120
 
 # Get documentserver healthcheck status
 echo "Wait for service wake up"
