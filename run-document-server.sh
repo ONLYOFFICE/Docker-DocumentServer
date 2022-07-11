65329cbd8be44f316004f314d61ee53bce8e2e2a
danilapog <danil.titarenko@onlyoffice.com>
2022-07-11 17:18:53 +0300

    Refactoring code

--- run-document-server.sh
+++ run-document-server.sh
@@ -173,7 +173,7 @@ update_supervisor_config() {
     if [[ -z ${RESULT} ]]; then
     for file in ${SUPERVISOR_CONF_DIR}/ds-converter.conf ${SUPERVISOR_CONF_DIR}/ds-docservice.conf
       do
-      sed -i -e "s|environment.*|&\,NODE_OPTIONS=${NODE_OPTIONS}|g" $file &> /dev/null
+      sed -i "s|environment.*|&\,NODE_OPTIONS=${NODE_OPTIONS}|g" $file &> /dev/null
       sed -i 's/\r//g' $file &> /dev/null
     done
     fi
