26e6cdde23404cffe04ff603463169952fe2965f
danilapog <danil.titarenko@onlyoffice.com>
2022-07-12 18:57:46 +0300

    Change if check key

--- run-document-server.sh
+++ run-document-server.sh
@@ -39,7 +39,7 @@ fi
 
 SSL_CERTIFICATES_DIR="/usr/share/ca-certificates/ds"
 mkdir -p ${SSL_CERTIFICATES_DIR}
-if [[ -f $DATA_DIR/certs ]]; then
+if [[ -d $DATA_DIR/certs ]]; then
   if [ "$(ls -A $DATA_DIR/certs)" ]; then
     cp -f ${DATA_DIR}/certs/* ${SSL_CERTIFICATES_DIR}
     chmod 644 ${SSL_CERTIFICATES_DIR}/*.crt ${SSL_CERTIFICATES_DIR}/*.pem
