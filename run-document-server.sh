c576b946f9c9362c7edd5b917e7f1a2390c9b2cc
evgeniy-antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2022-08-22 18:11:37 +0300

    Minor correction

--- run-document-server.sh
+++ run-document-server.sh
@@ -82,10 +82,11 @@ else
   JWT_ENABLED="false"
 fi
 
+[ -z $JWT_SECRET ] && JWT_MESSAGE="JWT is enabled by default. A random secret is generated automatically. Run the command 'docker exec $(sudo docker ps -q) sudo documentserver-jwt-status.sh' to get information about JWT."
+
 JWT_SECRET=${JWT_SECRET:-$(pwgen -s 20)}
 JWT_HEADER=${JWT_HEADER:-Authorization}
 JWT_IN_BODY=${JWT_IN_BODY:-false}
-[ -z $JWT_SECRET ] && JWT_MESSAGE="JWT is enabled by default. A random secret is generated automatically. Run the command 'docker exec $(sudo docker ps -q) sudo documentserver-jwt-status.sh' to get information about JWT."
 
 WOPI_ENABLED=${WOPI_ENABLED:-false}
 
