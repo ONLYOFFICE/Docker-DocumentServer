e44acbebf7a1be2cccda7a912d1f761310d865bc
Roman Demidov <roman.demidov@onlyoffice.com>
2022-08-31 12:13:30 +0300

    Fix KylinOS start error (#471)
    * Fix KylinOS start error
    
    * Small changes
    
    * Small changes

--- run-document-server.sh
+++ run-document-server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+umask 0022
+
 function clean_exit {
   /usr/bin/documentserver-prepare4shutdown.sh
 }
@@ -592,6 +594,8 @@ else
   
   update_welcome_page
 fi
+
+find /etc/${COMPANY_NAME} -exec chown ds:ds {} \;
 
 #start needed local services
 for i in ${LOCAL_SERVICES[@]}; do
