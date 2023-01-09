9a526077addbc87a4c6abf1eb6142256f7118f4f
danilapog <danil.titarenko@onlyoffice.com>
2023-01-09 15:04:23 +0300

    Update run command for disql copy

--- tests/damengdb/damengdb-get-disql.sh
+++ tests/damengdb/damengdb-get-disql.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Get disql for remote access
-  docker run -d -p 5236:5236 --restart=always --name dm8_01 --privileged=true -e PAGE_SIZE=16 -e LD_LIBRARY_PATH=/opt/dmdbms/bin -e INSTANCE_NAME=dm8_01 
+  docker run -d -p 5236:5236 --restart=always --name dm8_01 --privileged=true -e PAGE_SIZE=16 -e LD_LIBRARY_PATH=/opt/dmdbms/bin -e INSTANCE_NAME=dm8_01 dm8_single:v8.1.2.128_ent_x86_64_ctm_pack4
   echo "Disql bin will be copied from dm8 container. Please wait couple of minutes"
   sleep 60
   docker cp dm8_01:/opt/dmdbms/bin .
+  echo "OK: Files was copy"
   docker stop dm8_01 ; docker rm dm8_01
   echo "OK: Disql bin files ready"
