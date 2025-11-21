d3d6bcf8b4293bba46dd3cde70df643fffca8f42
Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
2025-11-21 12:58:45 +0300

    fix bug 78605 - Prevent SIGTERM shutdown hang on log tailing

--- run-document-server.sh
+++ run-document-server.sh
@@ -827,4 +827,4 @@ start_process documentserver-static-gzip.sh ${ONLYOFFICE_DATA_CONTAINER}
 
 echo "${JWT_MESSAGE}" 
 
-start_process find "$DS_LOG_DIR" "$DS_LOG_DIR-example" -type f -name "*.log" | xargs tail -F
+start_process bash -c "find '$DS_LOG_DIR' '$DS_LOG_DIR-example' -type f -name '*.log' | xargs tail -F"
